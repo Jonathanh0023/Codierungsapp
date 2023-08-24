eceea2ab917ddb11ea469d487aeefc00ed324598
Jonathanh0023 <142325028+Jonathanh0023@users.noreply.github.com>
2023-08-24 16:41:42 +0200

    Litapp.py

--- Litapp.py
+++ Litapp.py
@@ -26,24 +26,14 @@ def categorize_words(categories, search_words, question_template):
 # Streamlit interface
 st.title("Kategorien Klassifikator")
 
-# Divide the page into 3 columns
-col1, col2, col3 = st.columns(3)
-
-# Codes input
-col1.subheader("Codes:")
-category_numbers = col1.text_area("", "1\n2\n3")
-
-# Category names input
-col2.subheader("Kategorien:")
-category_names = col2.text_area("", 'Kategorie für Code 1\nKategorie für Code 2\nKategorie für Code 3')
-
-# Search words input
-col3.subheader("Suchwörter:")
-search_words = col3.text_area("", 'Offene Nennungen untereinander einfügen\n...\n...\n...')
-
-# Question template input
-st.subheader("Aufgabe für die KI (Wichtig: Nur bei Bedarf verändern! {word} muss im Satz bleiben):")
-question_template = st.text_area("", 'Zu welcher Kategorie oder welchen Kategorien gehören die Suchwörter? Antworte nur in Zahlen. {word}')
+# Input for categories
+category_numbers = st.text_area("Codes:", placeholder="1\n2\n3")
+category_names = st.text_area("Kategorien:", placeholder='Kategorie für Code 1\nKategorie für Code 2\nKategorie für Code 3')
+search_words = st.text_area("Suchwörter:", placeholder='Offene Nennungen untereinander einfügen\n...\n...\n...')
+question_template = st.text_area(
+    "Aufgabe für die KI (Wichtig: Nur bei Bedarf verändern! {word} muss im Satz bleiben):", 
+    placeholder='Zu welcher Kategorie oder welchen Kategorien gehören die Suchwörter? Antworte nur in Zahlen. {word}'
+)
 
 if st.button("Los gehts"):
     categories = [f"{num.strip()}: {name.strip()}" for num, name in zip(category_numbers.splitlines(), category_names.splitlines())]
