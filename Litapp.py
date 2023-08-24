424e7cf77ab6bf3f5e184c07cedd70c55dce45e5
Jonathanh0023 <142325028+Jonathanh0023@users.noreply.github.com>
2023-08-24 16:25:53 +0200

    Litapp.py

--- Litapp.py
+++ Litapp.py
@@ -1,36 +1,24 @@
-import streamlit as st
-import openai
-
-# Set the API key
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-
-def categorize_words(categories, search_words, question_template):
-    results = {}
-    category_string = ", ".join(categories)
-
-    for word in search_words:
-        question = question_template.format(word=word.strip())
-        prompt = f"{category_string}. {question}"
-
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[{"role": "user", "content": prompt}],
-            temperature=0.2,
-            max_tokens=50
-        )
-
-        results[word.strip()] = response.choices[0].message.content.strip()
-
-    return results
-
 # Streamlit interface
 st.title("Kategorien Klassifikator")
 
-# Input for categories
-category_numbers = st.text_area("Codes:", "1\n2\n3")
-category_names = st.text_area("Kategorien:", 'Kategorie für Code 1\nKategorie für Code 2\nKategorie für Code 3')
-search_words = st.text_area("Suchwörter:", 'Offene Nennungen untereinander einfügen\n...\n...\n...')
-question_template = st.text_area("Aufgabe für die KI (Wichtig: Nur bei Bedarf verändern! {word} muss im Satz bleiben):", 'Zu welcher Kategorie oder welchen Kategorien gehören die Suchwörter? Antworte nur in Zahlen. {word}')
+# Divide the page into 3 columns
+col1, col2, col3 = st.columns(3)
+
+# Codes input
+col1.subheader("Codes:")
+category_numbers = col1.text_area("", "1\n2\n3")
+
+# Category names input
+col2.subheader("Kategorien:")
+category_names = col2.text_area("", 'Kategorie für Code 1\nKategorie für Code 2\nKategorie für Code 3')
+
+# Search words input
+col3.subheader("Suchwörter:")
+search_words = col3.text_area("", 'Offene Nennungen untereinander einfügen\n...\n...\n...')
+
+# Question template input
+st.subheader("Aufgabe für die KI (Wichtig: Nur bei Bedarf verändern! {word} muss im Satz bleiben):")
+question_template = st.text_area("", 'Zu welcher Kategorie oder welchen Kategorien gehören die Suchwörter? Antworte nur in Zahlen. {word}')
 
 if st.button("Los gehts"):
     categories = [f"{num.strip()}: {name.strip()}" for num, name in zip(category_numbers.splitlines(), category_names.splitlines())]
@@ -40,4 +28,3 @@ if st.button("Los gehts"):
     st.subheader("Ergebnisse:")
     for word, category in results.items():
         st.write(f"{word}: {category}")
-
