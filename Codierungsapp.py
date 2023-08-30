0d682581a21748d2e77ca34b8a0a177ac5d98170
Jonathanh0023 <142325028+Jonathanh0023@users.noreply.github.com>
2023-08-30 10:50:10 +0200

    Add files via upload

---
+++ Codierungsapp.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import openai
+
+# Set the API key
+openai.api_key = "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"
+
+def categorize_words(categories, search_words, question_template):
+    results = {}
+    category_string = ", ".join(categories)
+
+    for word in search_words:
+        # Use the provided question template
+        question = question_template.format(word=word.strip())
+        prompt = f"{category_string}. {question}"
+
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {
+                "role": "user",
+                "content": prompt
+                }
+            ],
+            temperature=0.2,
+            max_tokens=50,
+           )
+
+        # Assuming the response directly contains the category integer
+        results[word.strip()] = response.choices[0].message.content.strip()
+
+    return results
+
+st.title("BonsAI Codierungstool")
+
+# Creating Streamlit widgets to capture input using columns
+col1, col2, col3 = st.columns([0.5, 3, 3])
+
+with col1:
+    category_numbers = st.text_area("Codes:", placeholder="1\n2\n3", height=600)
+with col2:
+    category_names = st.text_area("Kategorien:", placeholder='Kategorie für Code 1\nKategorie für Code 2\nKategorie für Code 3', height=600)
+with col3:
+    search_words = st.text_area("Suchwörter:", placeholder='Offene Nennungen untereinander einfügen\n...\n...\n...', height=600)
+
+question_template = st.text_area("Aufgabe für die KI (Wichtig: Nur bei Bedarf verändern! {word} muss im Satz bleiben):", 'Zu welcher Kategorie oder welchen Kategorien gehören die Suchwörter? Antworte nur in Zahlen. {word}')
+
+# Button to trigger the categorization
+if st.button("Los gehts"):
+    categories = [f"{num.strip()}: {name.strip()}" for num, name in zip(category_numbers.splitlines(), category_names.splitlines())]
+    results = categorize_words(categories, search_words.splitlines(), question_template)
+
+    st.subheader("Ergebnisse:")
+    for word, category in results.items():
+        st.write(f"**{word}**: {category}")
